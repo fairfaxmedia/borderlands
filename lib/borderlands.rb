393fea15e182079e9cdba63e536e7eca714e9e0d
John Slee <john.slee@fairfaxmedia.com.au>
2016-08-19 17:40:31 +1000

    display group and verdict for each property

--- lib/borderlands.rb
+++ lib/borderlands.rb
@@ -24,20 +24,26 @@ module Borderlands
       puts propertymanager.properties.sort.map { |p| "#{p.id} #{p.name}" }
     end
 
-    method_option :all,
-      type: :boolean,
-      default: false,
-      desc: 'display properties that have no hostname/DNS problems'
+    method_option :all, type: :boolean, default: false,
+      desc: 'list properties that have no DNS problems'
     desc 'audit_hostnames', 'examine all accessible properties for DNS anomalies'
     def audit_hostnames
-      format = "%-40s %-4s %-45s %-45s %-s"
-      puts sprintf format, *%w(PROPERTY PVER HOSTNAME EDGE-HOSTNAME DNS-STATUS)
+      groups = {}
+      propertymanager.groups.sort.each { |g| groups[g.id] = g.name.gsub(/\s+/,'_') }
+      format = "%-8s %-30s %-40s %-4s %-45s %-45s %-s"
+      puts sprintf format, *%w(VERDICT GROUP PROPERTY PVER HOSTNAME EDGE-HOSTNAME DNS-STATUS)
       propertymanager.properties.sort.each do |property|
+        group = groups[property.groupid]
         hostnames = propertymanager.hostnames(property)
         if hostnames
-          if options[:all] || ! hostnames.all? { |hostname| hostname.status.ok? }
+          verdict = 'PROBLEMS' if hostnames.any?  { |hostname| hostname.status.ok? }
+          verdict = 'DEAD'     if hostnames.none? { |hostname| hostname.status.ok? }
+          verdict = 'OK'       if hostnames.all?  { |hostname| hostname.status.ok? }
+          if options[:all] || verdict != 'OK'
             hostnames.each do |hostname|
               puts sprintf format,
+                verdict,
+                groups[property.groupid],
                 property.name,
                 property.productionversion, 
                 hostname.name,
@@ -47,6 +53,8 @@ module Borderlands
           end
         else
           puts sprintf format,
+            'DEAD',
+            group,
             property.name,
             property.productionversion || 'none',
             *%w(none none DNS:NONE)
