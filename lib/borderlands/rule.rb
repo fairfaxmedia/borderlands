13367532876ec857b8974b456d3a8de3706776cb
John Slee <john.slee@fairfaxmedia.com.au>
2016-09-22 18:42:39 +1000

    fix spelling, add ruletree walk operation

--- lib/borderlands/rule.rb
+++ lib/borderlands/rule.rb
@@ -4,13 +4,14 @@ require 'borderlands/criteria'
 
 module Borderlands
   class Rule < AkamaiObject
-    def initialize(data)
+    def initialize(data, options={})
       @id         = data['name'] # rules don't have an id field
       @name       = data['name']
       @options    = data['options']
-      @behaviours = []
+      @behaviors = []
+      @depth      = options[:depth] || 0
       if data['behaviors']
-        @behaviours = data['behaviors'].map do |bv|
+        @behaviors = data['behaviors'].map do |bv|
           Behavior.new bv
         end
       end
@@ -22,14 +23,36 @@ module Borderlands
       end
       if data['children'] && data['children'].size > 0
         @children = data['children'].map do |child|
-          Rule.new child
+          Rule.new(child, { :depth => @depth + 1 })
         end
       end
     end
+
     def is_secure
       options['is_secure']
     end
+
+    def to_pp
+      bn = @behaviors.map { |b| b.name }
+      cn = @criteria.map  { |c| c.name }
+      indent = "\t" * @depth
+      ppout = [ "#{indent}#{name}: criteria[#{cn.join(',')}], behaviours[#{bn.join(',')}]" ]
+      if @children
+        @children.each { |child| ppout << child.to_pp }
+      end
+      ppout.join("\n")
+    end
+
+    def walk(&block)
+      raise "expected block" unless block_given?
+      yield self
+      @children.each { |child| child.walk(&block) } if @children
+    end
+
     attr_reader :options
     attr_reader :behaviors
+    attr_reader :children
+    attr_reader :depth
   end
+
 end
